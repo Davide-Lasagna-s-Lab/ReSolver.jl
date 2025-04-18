4e406969b4de941463bcedcfd1ecaf4dffad6864
Thomas Burton <tburton5572@gmail.com>
2025-04-18 21:28:03 +0100

    altered residual interface to take periods explicitly, and added residual tests

--- src/residual.jl
+++ src/residual.jl
@@ -1,15 +1,6 @@
 # Definition of the interface to define the object that calculates the residuals
 # for a given generic input.
 
-
-# ~~~ Assumed interface for Residual ~~~
-#  - similar(::X) -> X
-#  - dot(::X, ::X) -> Number
-#  - norm(::X) -> Non-negative Real
-#  - broadcasting between variables of type X
-#  - get_period(::X) -> Real
-#  - set_period!(::X, Real) -> X
-
 # ! the base profile should be handled inside the navier-stokes operator call
 # ! any norm scaling should be handled inside the norm method and adjoint operator calls
 
@@ -17,53 +8,59 @@
 Some type
 """
 struct Residual{X, DT, NS, ADJ}
-            cache::NTuple{5, X}
-             ddt!::DT
-    rhs_operator!::NS
-    adj_operator!::ADJ
+    cache::NTuple{5, X}
+     dds!::DT
+     rhs!::NS
+     adj!::ADJ
 
     Residual(x::X, 
-             ddt!::DT,
-             rhs_operator!::NS,
-             adj_operator!::ADJ) where {X, DT, NS, ADJ} = new{X, DT, NS, ADJ}(ntuple(i->similar(x), 5),
-                                                                                     ddt!,
-                                                                                     rhs_operator!,
-                                                                                     adj_operator!)
+             dds!::DT,
+             rhs!::NS,
+             adj!::ADJ) where {X, DT, NS, ADJ} = new{X, DT, NS, ADJ}(ntuple(i->similar(x), 5),
+                                                                     dds!,
+                                                                     rhs!,
+                                                                     adj!)
 end
 
 """
 A call
 """
-function (f::Residual{X})(a::X) where {X}
+function (f::Residual{X})(x::X, T::Real) where {X}
     # aliases
-    dadt = f.cache[1]
-    N_a  = f.cache[2]
-    s    = f.cache[3]
+    dxds = f.cache[1]
+    N_x  = f.cache[2]
+    r    = f.cache[3]
+
+    # compute fundamental frequency
+    ω = 2π/T
 
     # compute local residual
-    s .= ddt!(dadt, a) .- f.rhs_operator!(N_a, a)
+    r .= ω.*f.dds!(dxds, x) .- f.rhs!(N_x, x)
 
-    return norm(s)^2/2
+    return norm(r)^2/2
 end
 
 """
 A second call
 """
-function (f::Residual{X})(dRdx::X, a::X) where {X}
+function (f::Residual{X})(dRdx::X, x::X, T::Real) where {X}
     # aliases
-    s     = f.cache[3]
-    dsdt  = f.cache[4]
-    M_a_s = f.cache[5]
+    dxds  = f.cache[1]
+    r     = f.cache[3]
+    drds  = f.cache[4]
+    M_x_r = f.cache[5]
+
+    # compute fundamental frequency
+    ω = 2π/T
 
     # compute residual
-    R = f(a)
+    R = f(x, T)
 
     # compute field gradient
-    dRdx .= f.adj_operator!(M_a_s, a, s) .- ddt!(dsdt, s)
+    dRdx .= f.adj!(M_x_r, x, r) .- ω.*f.dds!(drds, r)
 
     # compute frequency gradient
-    dRdT = dot(dadt, s)/get_period(a)
-    set_period!(dRdx, dRdT)
+    dRdT = (ω/T)*dot(dxds, r)
 
-    return dRdx, R
+    return R, dRdx, dRdT
 end
---
+++ test/test_residuals.jl
@@ -0,0 +1,20 @@
+@testset "Residuals interface       " begin
+    # test construction
+    RdR! = Residual([0.0],
+                    (dxdt, x)->(dxdt[1] = 0.0; dxdt),
+                    (F, x)->(F[1] = x[1]^2; F),
+                    (G, x, y)->(G[1] = -2*x[1]*y[1]))
+    @test RdR! isa Residual{Vector{Float64}}
+
+    # test correctness
+    dRdx = randn(1)
+    @test RdR!(dRdx, [0.0], rand()) == (0, [0.0], 0.0)
+    @test RdR!(dRdx, [1.0], rand()) == (0.5, [2.0], 0.0)
+    x = randn(1)
+    @test RdR!(dRdx, x, rand()) == (abs2(x[1]^2)/2, [2*x[1]^3], 0.0)
+
+    # test allocation
+    fun1(x, T) = @allocated RdR!(x, T)
+    fun2(dRdx, x, T) = @allocated RdR!(dRdx, x, T)
+    @test fun1(x, rand()) == fun2(dRdx, x, rand()) == 0
+end
