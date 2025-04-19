512b3887c81d7adcab07878e94b438820ac97971
Thomas Burton <tburton5572@gmail.com>
2025-04-19 15:20:58 +0100

    finished optimisation vector implementation and tested

--- src/ReSolver.jl
+++ src/ReSolver.jl
@@ -1,14 +1,14 @@
 module ReSolver
 
-using LinearAlgebra, Optim, Parameters, Printf
+using LinearAlgebra, Parameters, Printf#, Optim
 
 export Residual, optimise!
 
 include("residual.jl")
 include("optimvector.jl")
-include("options.jl")
-include("trace.jl")
-include("callback.jl")
-include("optimise.jl")
+# include("options.jl")
+# include("trace.jl")
+# include("callback.jl")
+# include("optimise.jl")
 
 end
--- src/optimvector.jl
+++ src/optimvector.jl
@@ -8,38 +8,58 @@
 # separate objects. This is also a good way to add in dependence on continuous
 # symmetries similar to MVector in NKSearch.jl.
 
-mutable struct OptimVector{X} <: AbstractVector{Float64}
+# ~~~ Wrapper vector struct for optimisation ~~~
+mutable struct OptimVector{X, N} <: AbstractVector{Float64}
     const x::X
-          T::Vector{Float64}
+          T::Float64
     
-    OptimVector(x::X, T::Real) where {X} = new{X}(x, convert(Float64, T))
+    OptimVector(x::X, T::Real) where {X} = new{X, length(x) + 1}(x, convert(Float64, T))
 end
 
-# interface methods
-Base.eltype(x::OptimVector) = eltype(x.x)
-Base.similar(x::OptimVector, ::Type{T}) where {T} = OptimVector(similar(x.x, T), x.T)
 
-# broadcasting
+# ~~~ interface methods ~~~
+Base.IndexStyle(::Type{<:OptimVector})            = IndexLinear()
+Base.size(::OptimVector{X, N}) where {X, N}       = (N,)
+Base.eltype(x::OptimVector)                       = eltype(x.x)
+Base.similar(x::OptimVector, ::Type{T}) where {T} = OptimVector(similar(x.x, T), 0.0)
+Base.copy(x::OptimVector)                         = OptimVector(copy(x.x), x.T)
+
+# storing "N" as parametric type allows compiler to optimise away the if-else blocks through constant propagation
+@inline function Base.getindex(x::OptimVector{X, N}, i::Int) where {X, N}
+    if i < N
+        @boundscheck checkbounds(x.x, i)
+        @inbounds val = x.x[i]
+    elseif i == N
+        val = x.T
+    else
+        throw(BoundsError(x, i))
+    end
+    return val
+end
+
+
+# ~~~ broadcasting ~~~
 const OptimVectorStyle = Base.Broadcast.ArrayStyle{OptimVector}
 Base.BroadcastStyle(::Type{<:OptimVector}) = OptimVectorStyle()
 
 @inline Base.similar(bc::Base.Broadcast.Broadcasted{OptimVectorStyle}, ::Type{T}) where {T} = similar(find_opvec(bc), T)
 
 find_opvec(bc::Base.Broadcast.Broadcasted) = find_opvec(bc.args)
-find_opvec(args::Tuple) = find_opvec(find_opvec(args[1]), Base.tail(args))
-find_opvec(x) = x
-find_opvec(::Tuple{}) = nothing
-find_opvec(x::OptimVector, rest) = x
-find_opvec(::Any, rest) = find_opvec(rest)
-
-@inline Base.copy(bc::Base.Broadcast.Broadcasted{OptimVectorStyle}) = copyto!(des, similar(bc, eltype(find_opvec(bc))))
+find_opvec(args::Tuple)                    = find_opvec(find_opvec(args[1]), Base.tail(args))
+find_opvec(x)                              = x
+find_opvec(::Tuple{})                      = nothing
+find_opvec(x::OptimVector, rest)           = x
+find_opvec(::Any, rest)                    = find_opvec(rest)
 
 @inline function Base.copyto!(dest::OptimVector, bc::Base.Broadcast.Broadcasted{OptimVectorStyle})
+    # flatten nested broadcasting representation
+    bcf = Base.Broadcast.flatten(bc)
+
     # broadcast function over all the main vector arguments
-    Base.Broadcast.broadcast!(bc.f, dest.x, map(_get_x, bc.args)...)
+    Base.Broadcast.broadcast!(bcf.f, dest.x, map(_get_x, bcf.args)...)
 
     # broadcast function ovar all the period arguments
-    dest.d = Base.Broadcast.broadcast(bc.f, map(_get_T, bc.args))
+    dest.T = Base.Broadcast.broadcast(bcf.f, map(_get_T, bcf.args)...)
 
     return dest
 end
